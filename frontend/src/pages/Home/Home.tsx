f6d6156e9914f49da2699f6fbbd38b33881bda0d
Diwan Malla <diwanmalla@192-168-1-3.tpgi.com.au>
2024-08-10 16:22:30 +1000

    Created notecard and remove Login component from home and added Navbar and Notecard component with button to add

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,28 @@
-import Login from "../Login/Login";
+import { MdAdd } from "react-icons/md";
+import NoteCard from "../../components/Cards/NoteCard";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
-      <Login />
+      <Navbar />
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Meeting on 10 aug"
+            date="3rd Apr 2024"
+            content="Meeting on 7th April Meeting on 7th april"
+            tags="#meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 lg:bottom-10">
+        <MdAdd className="text-[32px] text-white" />
+      </button>
     </div>
   );
 };
